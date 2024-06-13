da6da64ce055cbc8b09eab0d9529be7d8c08bea4
Brian Bossy  Ondieki <brianbossy254@gmail.com>
2024-06-13 23:33:19 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 function Connection() {
-    const mongoURI = "mongodb://127.0.0.1:27017/chat"
+    const mongoURI = "mongodb://localhost:27017/chat"
     mongoose.connect(mongoURI)
     .then(() => console.log("connected"))
     .catch(err => console.log(err))
